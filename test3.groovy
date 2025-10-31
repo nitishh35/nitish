178d985b604da69045f4926d3f6d7dd26b3fb721
nitishh35 <nitishh35@gmail.com>
2025-10-31 18:44:04 +0530

    Update test3.groovy

--- test3.groovy
+++ test3.groovy
@@ -69,3 +69,98 @@ def call(String repositoryName) {
             <tr><td>qa-west2</td><td>${qaWest2Version}</td><td>uat-west2</td><td>${uatWest2Version}</td></tr>
             <tr><td>qa1-east1</td><td>${qa1East1Version}</td><td>uat1-east1</td><td>${uat1East1Version}</td></tr>
             <tr>
+
+
+-----------------------------------------------------
+
+
+from calaude
+
+// ===================================================================
+// FILE 1: vars/getUDeployVersions.groovy (in your shared library repo)
+// ===================================================================
+
+import jenkins.*
+import jenkins.model.*
+import hudson.*
+import hudson.model.*
+import groovy.json.JsonSlurper
+
+def call(String repositoryName) {
+    def jenkinsCredentials = com.cloudbees.plugins.credentials.CredentialsProvider.lookupCredentials(
+        com.cloudbees.plugins.credentials.Credentials.class,
+        Jenkins.instance,
+        null,
+        null
+    )
+    
+    for(creds in jenkinsCredentials) {
+        if(creds.id == "UDEPLOY_PROD_CREDENTIALS") {
+            def versions = fetchAllVersions(creds, repositoryName)
+            return generateHtmlTable(versions)
+        }
+    }
+    return "Credentials not found"
+}
+
+def fetchAllVersions(creds, repositoryName) {
+    def versions = [:]
+    def environments = [
+        'qa2dal', 'qa3dal', 'qa2phx', 'qa-east1', 'qa-west2', 'qa1-east1', 'qa1-east2',
+        'uat2dal', 'uat3dal', 'uat2phx', 'uat-east1', 'uat-west2', 'uat1-east1', 'uat1-east2'
+    ]
+    
+    environments.each { env ->
+        def response = ("curl -ku ${creds.username}:${creds.password} " +
+            "https://udeploy.app.syfbank.com:8443/cli/component/getProperty?" +
+            "component=Ecom-API_${repositoryName}&name=last-deployed-${env}-version").execute()
+        
+        def responseText = response.text
+        versions[env] = responseText.contains('Property not found') ? 'NA' : responseText
+    }
+    
+    return versions
+}
+
+def generateHtmlTable(versions) {
+    def html = """
+    <html>
+    <head>
+        <style>
+            table {
+                font-family: arial, sans-serif;
+                border-collapse: collapse;
+                width: 50%;
+            }
+            td, th {
+                border: 1px solid #dddddd;
+                text-align: center;
+                padding: 12px;
+            }
+        </style>
+    </head>
+    <body>
+        <table>
+            <tr><th colspan='2'>QA</th><th colspan='2'>UAT</th></tr>
+            <tr><td>qa2-dal</td><td>${versions['qa2dal']}</td><td>uat2-dal</td><td>${versions['uat2dal']}</td></tr>
+            <tr><td>qa3-dal</td><td>${versions['qa3dal']}</td><td>uat3-dal</td><td>${versions['uat3dal']}</td></tr>
+            <tr><td>qa2-phx</td><td>${versions['qa2phx']}</td><td>uat2-phx</td><td>${versions['uat2phx']}</td></tr>
+            <tr><td>qa-east1</td><td>${versions['qa-east1']}</td><td>uat-east1</td><td>${versions['uat-east1']}</td></tr>
+            <tr><td>qa-west2</td><td>${versions['qa-west2']}</td><td>uat-west2</td><td>${versions['uat-west2']}</td></tr>
+            <tr><td>qa1-east1</td><td>${versions['qa1-east1']}</td><td>uat1-east1</td><td>${versions['uat1-east1']}</td></tr>
+            <tr><td>qa1-east2</td><td>${versions['qa1-east2']}</td><td>uat1-east2</td><td>${versions['uat1-east2']}</td></tr>
+        </table>
+    </body>
+    </html>
+    """
+    return html
+}
+
+// ===================================================================
+// FILE 2: In Jenkins Job - Active Choice Parameter (Groovy Script)
+// ===================================================================
+
+@Library('your-shared-library-name') _
+
+// Simply call the shared library function
+return getUDeployVersions(REPOSITORY_NAME)
