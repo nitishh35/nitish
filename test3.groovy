024ee99534b4602fef358f048c38519cfd5b89ab
nitishh35 <nitishh35@gmail.com>
2025-11-21 15:50:31 +0530

    Update test3.groovy

--- test3.groovy
+++ test3.groovy
@@ -1,9 +1,1 @@
-dev.devops_buildinfo_template_json={
-  "title": "Jenkins Build Notification",
-  "summary": "Pipeline Execution Update",
-  "buildUrl": "<BUILD_URL>",
-  "triggeredBy": "<TRIGGERED_BY>",
-  "triggeredByEmail": "<TRIGGERED_BY_EMAIL>",
-  "status": "<STATUS>",
-  "message": "A pipeline event occurred in the DEV environment."
-}
+dev.devops_buildinfo_template_json={"title":"Jenkins Build Notification","summary":"Pipeline Execution Update","buildUrl":"<BUILD_URL>","triggeredBy":"<TRIGGERED_BY>","triggeredByEmail":"<TRIGGERED_BY_EMAIL>","status":"<STATUS>","message":"A pipeline event occurred in the DEV environment."}
